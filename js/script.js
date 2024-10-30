21c5aaa6908957085b8a203df6532876cb7e5052
Aziz <baziz1275@gmail.com>
2024-10-30 11:18:46 +0600

    Variables and data types

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var nam = "AMJLT Cyber School"
+
+if(true){
+    var nam = "Aziz"
+    console.log(nam); //Aziz
+}
+console.log(nam); 
+
+var nam = "Maga"
+console.log(nam);
+
+let name = "Dedpool"
+if(true){
+    let name = "AMJ"
+    console.log(name);
+}
+
+console.log("жалпы аты " + name);
+
+const text = "Efrat"
+
+console.log(" Menin atym" + text);
+console.log("my name is" + text);
+
+let age = 5
+let age2 = 20
+console.log(age + age2);
+
+
+
+
+
